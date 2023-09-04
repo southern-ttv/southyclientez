69e88722cdb124743b55b92c0ce7db30e2f5fb32
Southy <84360747+southern-ttv@users.noreply.github.com>
2023-09-04 11:54:51 -0500

    Add files via upload

---
+++ southyspp.lua
@@ -0,0 +1,10 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "Southys Premium Hecker Menu", HidePremium = false, SaveConfig = true, IntroEnabled = true, IntroText = "Southys Hecker Client"})
+
+local Tab = Window:MakeTab({
+	Name = "Home",
+	Icon = "nil",
+	PremiumOnly = false
+})
+
